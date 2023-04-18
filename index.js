91f993665ca6631340a2edb04eb90a233446ec79
Isaiah G <isaiahrg12@gmail.com>
2023-04-18 09:03:40 -0500

    Check user inputs
    Validating if user 'operations' and 'integers' are valid

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var readlineSync = require("readline-sync");
+let readlineSync = require("readline-sync");
 
-// prompt the name question
 // 1.) Ask the user, "What operation would you like to perform?"
 // 2.) Then the user enters one of these options: "/" "*" "-" "+"
 // 3.) If the user enters an invalid character, print: "That is not a valid operation" and then restart the program
@@ -9,14 +8,51 @@ var readlineSync = require("readline-sync");
 // 6.) After a valid number is entered, ask the user, "Please enter the second number". Perform the same error handling as before
 // 7.) Then create a function to perform the proper math operation and print the result as: "The result is: X" where "X" is the actual result
 
-// Wait for user's response.
 // prompt the name question
-var userName = readlineSync.question("May I have your name? ");
+// Wait for user's response.
+let userName = readlineSync.question("May I have your name? ");
 console.log("Hi " + userName + "!");
 
 //
 
-var whatOperation = readlineSync.question(
-  "What operation would you like to perform? "
+let operations = [];
+let whatOperation = readlineSync.question(
+  `What operation would you like to perform ${userName}? `
 );
-console.log(whatOperation);
+// console.log(whatOperation);
+operations.push(whatOperation);
+console.log(operations);
+
+for (let op of operations) {
+  switch (op) {
+    case "/":
+      console.log("Division it is!");
+      break;
+    case "*":
+      console.log("multiplication it is!");
+      break;
+    case "+":
+      console.log("addition it is!");
+      break;
+    case "-":
+      console.log("subtraction it is!");
+      break;
+    default:
+      console.log(
+        `Sorry, ${op} is an invalid operation. Please enter one of the following: '/', '*', '+', '-'.`
+      );
+  }
+}
+
+integers = [];
+let firstInt = readlineSync.questionInt(`Please enter the first number. `);
+console.log(firstInt);
+integers.push(firstInt);
+
+for (let int of integers) {
+  if (Number.isInteger(parseInt(int))) {
+    console.log("This is an integer");
+  } else {
+    console.log("Invalid input. Please input an integer.");
+  }
+}
